7f9b80ff116b0303430cbe8206ab04678eff8b1f
forseticn <63159639+forseticn@users.noreply.github.com>
2021-02-23 21:37:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
 # passwall依赖
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+src-git small https://github.com/kenzok8/small
 
 #安装New Argon
 rm -rf ./package/lean/luci-theme-argon
